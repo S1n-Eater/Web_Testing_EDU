a3f4a2119932d59f92b7b64358095a4601c7a58c
Undertaker <64604463+Undertaker-0@users.noreply.github.com>
2023-08-26 19:14:52 +0300

    Minor changes

--- src/main/java/TestingPage/DropdownsPage/Dropdowns.java
+++ src/main/java/TestingPage/DropdownsPage/Dropdowns.java
@@ -3,7 +3,6 @@ package TestingPage.DropdownsPage;
 import TestingPage.PageObject.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class Dropdowns extends PageObject {
