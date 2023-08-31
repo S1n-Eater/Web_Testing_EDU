509c9a292fdaf92cc2bebc57fc45895aa7fbf61c
Undertaker <64604463+Undertaker-0@users.noreply.github.com>
2023-08-31 22:24:21 +0300

    - Add TestNG in dependencies from pom file
    - Add TestNG assertion for checkboxes test

--- src/main/java/TestingPage/CheckboxesPage/CheckboxesPage.java
+++ src/main/java/TestingPage/CheckboxesPage/CheckboxesPage.java
@@ -3,6 +3,7 @@ package TestingPage.CheckboxesPage;
 import TestingPage.PageObject.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class CheckboxesPage extends PageObject {
 
@@ -16,12 +17,15 @@ public class CheckboxesPage extends PageObject {
         Boolean selectCheker = false;
         int checkboxesCounter = 0;
 
+        Assert.assertFalse(selectCheker);
         System.out.println("Before the test\t|\tFirst checkbox is selected: " + selectCheker);
         driver.findElement(By.xpath("(//input[@type=\'checkbox\'][1])")).click();
         selectCheker = driver.findElement(By.xpath("(//input[@type=\'checkbox\'][1])")).isSelected();
+        Assert.assertTrue(selectCheker);
         System.out.println("After the test\t|\tFirst checkbox is selected: " + selectCheker);
 
         checkboxesCounter = driver.findElements(By.xpath("//input[@type=\'checkbox\']")).size();
+        Assert.assertEquals(checkboxesCounter,100);
         System.out.println("\nAll amount of checkboxes: \t" + checkboxesCounter);
     }
 }
